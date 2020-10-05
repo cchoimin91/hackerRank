f219dd6222ccbe767cb6f84b1c921e6ea5a82fac
cchoimin91 <cchoimin1991@gmail.com>
2020-10-05 22:50:09 +0900

    StringReverse

--- src/com/cm/Main.java
+++ src/com/cm/Main.java
@@ -3,8 +3,8 @@ package com.cm;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println(">>>결과");
-	    new JavaType();
-
+	    System.out.println(">>> 실행");
+	    new StringReverse();
+        System.out.println(">>> 종료");
     }
 }
