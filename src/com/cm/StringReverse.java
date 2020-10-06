d8520429b904b2635c238abb4491df48aaa80232
cchoimin91 <cchoimin1991@gmail.com>
2020-10-06 23:22:52 +0900

    StringReverse solution2

--- src/com/cm/StringReverse.java
+++ src/com/cm/StringReverse.java
@@ -1,4 +1,42 @@
 package com.cm;
 
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/java-string-reverse
+ */
 public class StringReverse {
+
+    public StringReverse() {
+        //solution();
+        solution2();
+    }
+
+    public void solution(){
+        Scanner sc=new Scanner(System.in);
+        String a =sc.next();
+
+        StringBuilder sb = new StringBuilder(a);
+        sb.reverse();
+
+        System.out.println(a.equals(sb.toString())?"Yes":"No");
+    }
+
+    public void solution2() {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        for (int startIdx = 0, endIdx = a.length()-1 ; startIdx < a.length(); startIdx++, endIdx--) {
+            if (a.charAt(startIdx) != a.charAt(endIdx)) {
+                System.out.println("No");
+                break;
+            }
+
+            // input = "a"  startIdx == endIdx
+            if(startIdx > endIdx || startIdx == endIdx){
+                System.out.println("Yes");
+                break;
+            }
+        }
+    }
 }
