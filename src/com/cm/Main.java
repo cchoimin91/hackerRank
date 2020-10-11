c1c1182b4160cf3dcba8daad3d9b2faba89fc5a3
cchoimin91 <cchoimin1991@gmail.com>
2020-10-11 22:31:27 +0900

    primality

--- src/com/cm/Main.java
+++ src/com/cm/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println(">>> 실행");
-	    new StringReverse();
+
+	    new Primality();
+
         System.out.println(">>> 종료");
     }
 }
